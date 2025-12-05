fefdc821a89f08a1fd00845b01184cf9a342ace5
Gaurab sapkota <iamgaurabsap20@gmail.com>
2025-12-05 22:44:47 +0545

    Fix issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -82,7 +82,7 @@ const Footer = () => {
             <div className="space-y-3">
               <div className="flex items-center text-blue-200">
                 <FaMapMarkerAlt className="mr-3" />
-                <span>munal chowk, Bharatpur, Nepal</span>
+                <span>Bharatpur 12, chitwan</span>
               </div>
               <div className="flex items-center text-blue-200">
                 <FaPhone className="mr-3" />
